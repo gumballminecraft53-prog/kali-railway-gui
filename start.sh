6ad589af55e002c5b8a8cfe13fc2e51205d87de3
gumballminecraft53-prog <gumballminecraft53@gmail.com>
2026-09-18 13:27:30 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ~/.vnc
+echo "password" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+
+rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
+
+vncserver :1 -geometry 1280x720 -depth 24
+
+PORT="${PORT:-8080}"
+websockify --web /usr/share/novnc/ --wrap-mode=ignore $PORT localhost:5901
